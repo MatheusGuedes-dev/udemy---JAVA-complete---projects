048d7bd32f7ba0defb9540d140487be798eab499
matheusldsguedes <134342650+matheusldsguedes@users.noreply.github.com>
2025-11-25 15:16:52 -0300

    Seller entity class

--- java JDBC project/src/aplication/program.java
+++ java JDBC project/src/aplication/program.java
@@ -1,14 +1,20 @@
 package aplication;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department (1, "books");
+		Seller seller = new Seller(21, "matheus", "essemesmo@gmail.com", LocalDate.of(2004, 02, 13), 1800.0, obj);
 		
-		System.out.println(obj);
+		System.out.println(seller);
+		
+		
 
 	}
 
