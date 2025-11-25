1143459ae5143e146e3a9bd0e33c9bacedbe4b38
matheusldsguedes <134342650+matheusldsguedes@users.noreply.github.com>
2025-11-25 16:04:48 -0300

    SellerDaoJDBC and DaoFactory

--- java JDBC project/src/aplication/program.java
+++ java JDBC project/src/aplication/program.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class program {
 		
 		Department obj = new Department (1, "books");
 		Seller seller = new Seller(21, "matheus", "essemesmo@gmail.com", LocalDate.of(2004, 02, 13), 1800.0, obj);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
