2f46b5aff3c65b78738c156d70f8ed5b5e374122
MatheusGuedes-dev <matheus.luis1302@gmail.com>
2025-12-03 17:18:11 -0300

    OrderStatus enum

--- course JPA/src/main/java/com/educandoweb/course/entities/Order.java
+++ course JPA/src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enus.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -26,6 +27,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant date;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,10 +36,11 @@ public class Order implements Serializable{
 	public Order() {}
 	
 
-	public Order(Long id, Instant date, User client) {
+	public Order(Long id, Instant date, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.date = date;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -58,6 +62,19 @@ public class Order implements Serializable{
 
 	public void setDate(Instant date) {
 		this.date = date;
+	}
+
+	
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus!= null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 
