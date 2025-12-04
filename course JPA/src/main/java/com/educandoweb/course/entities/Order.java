a2862995d45a97f9d6213c472c522b7347ef7f1f
MatheusGuedes-dev <matheus.luis1302@gmail.com>
2025-12-04 10:22:25 -0300

    OrderItem, many-to-many association with extra attributes

--- course JPA/src/main/java/com/educandoweb/course/entities/Order.java
+++ course JPA/src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enus.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,6 +31,9 @@ public class Order implements Serializable{
 	private Instant date;
 	
 	private Integer orderStatus;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -64,7 +70,9 @@ public class Order implements Serializable{
 		this.date = date;
 	}
 
-	
+	public Set<OrderItem> getItems(){
+		return items;
+	}
 	
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
